e3ef3aaf582c0bb7c8f5365eed5408e55a4cb8fc
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-20 08:46:34 -0700

    unit tests and integration test

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-"""Fixtures for unit and integration tests."""
+"""
+Fixtures for testing GithubOrgClient.
+"""
 
 org_payload = {
     "login": "test_org",
-    "id": 1,
+    "id": 123456,
     "url": "https://api.github.com/orgs/test_org",
     "repos_url": "https://api.github.com/orgs/test_org/repos"
 }
 
 repos_payload = [
-    {"id": 101, "name": "repo1", "license": {"key": "apache-2.0"}},
-    {"id": 102, "name": "repo2", "license": {"key": "mit"}},
-    {"id": 103, "name": "repo3", "license": {"key": "apache-2.0"}},
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
 ]
 
 expected_repos = ["repo1", "repo2", "repo3"]
 
 apache2_repos = [
-    {"id": 101, "name": "repo1", "license": {"key": "apache-2.0"}},
-    {"id": 103, "name": "repo3", "license": {"key": "apache-2.0"}},
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
 ]
