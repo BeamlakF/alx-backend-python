0da894292d45f5f9af2748b04fb5616499efb630
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-20 08:09:43 -0700

    unit tests and integration test

---
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Fixtures for testing GitHubOrgClient."""
+
+org_payload = {
+    "login": "test_org",
+    "id": 123456,
+    "url": "https://api.github.com/orgs/test_org"
+}
+
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
+]
+
+expected_repos = ["repo1", "repo2", "repo3"]
+
+apache2_repos = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
+]
