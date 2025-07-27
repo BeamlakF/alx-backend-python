f42a49f027e291b17f2993722a895c23307b097f
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-27 01:17:43 -0700

    edit again

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,10 +1,7 @@
+# chats/views.py
 from rest_framework import viewsets
-from .models import User, Conversation, Message
-from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
