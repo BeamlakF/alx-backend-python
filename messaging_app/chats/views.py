9ca4fcfb07d2319d3156d21675b7e3b21985979a
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-27 01:03:18 -0700

    edit again

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all().order_by('-created_at')
+    serializer_class = ConversationSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().order_by('-sent_at')
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        # Automatically associate the sender as the current user if needed
+        serializer.save(sender=self.request.user)
+from rest_framework.permissions import IsAuthenticated
+
+class MessageViewSet(viewsets.ModelViewSet):
+    ...
+    permission_classes = [IsAuthenticated]
