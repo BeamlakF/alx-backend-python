c04a9bc732fde00c7322645f239dbb9aaef839d5
BeamlakF <beamlakfekadu562@gmail.com>
2025-08-03 17:18:47 +0300

    week 6

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models import Message, Notification
 from .models import MessageHistory
-from .models import Reaction
 
 
 
 admin.site.register(MessageHistory)
 admin.site.register(Message)
 admin.site.register(Notification)
-admin.site.register(Reaction)
 
