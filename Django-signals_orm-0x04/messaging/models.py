1a81f2247f862ad07c8f6122cfbd1fe4c5780694
BeamlakF <beamlakfekadu562@gmail.com>
2025-08-03 17:22:36 +0300

    week 6

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UnreadMessagesManager(models.Manager):
+        def for_user(self, user):
+            return self.get_queryset().filter(receiver=user, read=False).only('id', 'content', 'sender', 'created_at')
+
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
@@ -9,6 +15,9 @@ class Message(models.Model):
     edited = models.BooleanField(default=False)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    objects = models.Manager()  
+    unread = UnreadMessagesManager()
     
     parent_message = models.ForeignKey(
         'self',  # self-referential
@@ -24,6 +33,8 @@ class Message(models.Model):
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver}"
+    
+
     
 class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
