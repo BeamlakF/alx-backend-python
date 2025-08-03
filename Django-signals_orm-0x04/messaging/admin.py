039940443058487ff1bea38cb01d417c75aad434
BeamlakF <beamlakfekadu562@gmail.com>
2025-08-03 16:47:57 +0300

    week 6

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Message, Notification
 from .models import MessageHistory
 
+
+
 admin.site.register(MessageHistory)
 admin.site.register(Message)
 admin.site.register(Notification)
+
--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -15,6 +15,8 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
 
     def __str__(self):
         return f"History for message {self.message.id} at {self.edited_at}"
