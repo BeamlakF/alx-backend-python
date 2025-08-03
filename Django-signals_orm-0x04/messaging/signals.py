a756d13f5ebf090f90664c8fd2b2c24020a011f7
BeamlakF <beamlakfekadu562@gmail.com>
2025-08-03 16:53:02 +0300

    week 6

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -3,6 +3,8 @@ from django.dispatch import receiver
 from .models import Message, Notification
 from .models import Message, MessageHistory
 from django.db.models.signals import pre_save
+from django.db.models.signals import post_delete
+from django.contrib.auth.models import User
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -30,3 +32,14 @@ def log_message_edit(sender, instance, **kwargs):
             old_content=old_message.content
         )
         instance.edited = True
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete all messages sent or received by the user
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    # Delete all notifications for the user
+    Notification.objects.filter(user=instance).delete()
+    # Delete all message histories for messages linked to the user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+
+@login_required
+@require_POST
+def delete_user(request):
+    user = request.user
+    user.delete()
+    return JsonResponse({'message': 'User and related data deleted successfully.'})
